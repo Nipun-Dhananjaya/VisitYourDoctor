881b71c337ef82ea1cec69699dc3f6c3b1fbadfe
Nipun-Dhananjaya <nipundhananjaya967@gmail.com>
2024-01-09 12:50:22 +0530

    my appointments,search results and top specialication pages finish

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,1 @@
-var dropdown = document.getElementById('dropdown');
-var labelToUpdate = document.getElementById('labelText');
 
-dropdown.addEventListener('change', function() {
-    labelToUpdate.textContent ="Search by "+ dropdown.options[dropdown.selectedIndex].text;
-});
